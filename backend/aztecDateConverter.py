18c01154bd1c3d04c4817ba32a2b6a2becf5ec4e
AdamGonz <adaxgonz@ucsc.edu>
2025-06-22 14:40:47 -0700

    adas
    sda

--- backend/aztecDateConverter.py
+++ backend/aztecDateConverter.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from calendar_data import nahuatl_numbers, xiuhpohualli_months
+
 
 def convert_to_aztec(date_str):
     """
@@ -13,8 +15,7 @@ def convert_to_aztec(date_str):
     date = datetime.strptime(date_str, "%Y-%m-%d")
     year_number, year_symbol = get_aztec_year_sign(date.year)
     month = get_xiuhpohualli_month(date_str)
-
-
+    
     return {
         "gregorian": date_str,
         "aztec_year_number": year_number,
@@ -55,22 +56,7 @@ def get_nahuatl_number_name(number):
     Outputs:
         str: The corresponding Nahuatl name (e.g., 'Ce', 'Ome', etc.)
     """
-    nahuatl_numbers = {
-        1: "Ce",
-        2: "Ome",
-        3: "Yei",
-        4: "Nahui",
-        5: "Macuili",
-        6: "Chicoaze",
-        7: "Chicome",
-        8: "Chicyei",
-        9: "Chicnahui",
-        10: "Matlactli",
-        11: "Matlactli ihuan Ce",
-        12: "Matlactli ihuan Ome",
-        13: "Matlactli ihuan Yei"
-    }
-    
+
     return nahuatl_numbers.get(number, "Unknown")
 
 def get_xiuhpohualli_month(date_obj):
@@ -87,27 +73,6 @@ def get_xiuhpohualli_month(date_obj):
             'english_translation': str
         }
     """
-    xiuhpohualli_months = [
-        ((3, 12), (3, 31), "Atlacahualo", "Lo Dejado por las aguas", "What is left by the water"),
-        ((4, 1), (4, 20), "Tlacaxipehualiztli", "El cambio en la Persona", "The Change in the Person"),
-        ((4, 21), (5, 10), "Tozoztontli", "La Pequeña velacion", "The Small Vigil"),
-        ((5, 11), (5, 30), "Hueytozoztli", "La gran velacion", "The Big Vigil"),
-        ((5, 31), (6, 19), "Toxcatl", "Las cosas Secas", "The Dry things"),
-        ((6, 20), (7, 9), "Etzacualiztli", "Comer frijol y maiz", "The feast of beans & corn"),
-        ((7, 10), (7, 29), "Tecuilhuitontli", "Fiesta de los Señores", "Fiesta of the Great people"),
-        ((7, 30), (8, 18), "Huey Tecuilhuitl", "La Gran Fiesta de los Señores", "The Big fiesta of the great people"),
-        ((8, 19), (9, 7), "Tlaxochimaco", "Ofrenda de Flores", "Offering of the Flowers"),
-        ((9, 8), (9, 27), "Xocotl Huetzi", "La Caída de los Frutos", "The Falling of the Fruit"),
-        ((9, 28), (10, 17), "Ochpaniztli", "Barrimiento de los Caminos", "The Sweeping of the Roads"),
-        ((10, 18), (11, 6), "Teotleco", "La Llegada de los principios generadores de la Naturaleza", "The Arriving of the principle generators of Nature"),
-        ((11, 7), (11, 26), "Tepeilhuitl", "La Fiesta de los Cerros", "The Fiesta of the Mountains"),
-        ((11, 27), (12, 16), "Quicholli", "La Veintena del Flamingo", "The 20 day count of the Flamingo"),
-        ((12, 17), (1, 5), "Panquetzaliztli", "El Levantamiento de los Estandartes", "The Raising of the Standards"),
-        ((1, 6), (1, 25), "Atemoztli", "El Descenso de la Aguas", "The descent of the water"),
-        ((1, 26), (2, 14), "Tititl", "Veintena del Recogimiento", "The 20 day count of Absorption"),
-        ((2, 15), (3, 6), "Izcalli", "Veintena del Resurgimiento", "The 20 day count of Resurgence"),
-        ((3, 7), (3, 11), "Nemontemi", "Tiempo de Reflexión", "5.25 days for Reflection")
-    ]
 
     date_obj = datetime.strptime(date_obj, "%Y-%m-%d").date()
 
